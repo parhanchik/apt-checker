223ae2d4d19590d9002d45433338015ac970e316
20074179 <PVMardirosyan@sberbank.ru>
2022-11-17 23:19:38 +0300

    first stability version

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Process:
         self.warning = []
         self.critical = []
 
-    def get_connection(self, process):
+    def _get_connection(self, process):
         try:
             output = ""
             res = process.connections()
@@ -25,50 +25,65 @@ class Process:
                     output += f"Network connection:\n\t\tip:\t{pcon.raddr.ip}:{pcon.raddr.port}\n"
 
                     # get domain name
-                    domain = os.popen(f"dig -x {pcon.raddr.ip} +short | awk -F '.' '{{print $0}}'").read()[:-1]
-                    output += f"\t\tdomain:\t{domain}\n"
+                    domain = os.popen(f"dig -x {pcon.raddr.ip} +short | awk -F '.' '{{print $0}}'").read()[:-2]
+                    if domain and "connection timed out" not in domain:
+                        output += f"\t\tdomain:\t{domain}\n"
 
                     try:
                         # get provider name
                         whois_output = os.popen(f'whois {pcon.raddr.ip}').read()
                         provider_name = re.search('OrgName:( *)(.*)', whois_output).group(2)
                         output += f"\t\tprovider name:\t{provider_name}\n"
-                    except Exception:
+                    except:
                         pass
                     return output
             return "\n"
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             return "\n"
 
-    def _get_all(self):
+    @staticmethod
+    def _get_all():
         return set([process for process in psutil.process_iter()])
 
-    def get_process_name(process):
+    def _get_name(self, process):
         try:
-            processName = process.name()
-            processID = process.pid
-            return f"name: {processName}\npid: {processID}"
+            proc_name = process.name()
+            proc_id = process.pid
+            return f"name: {proc_name}\npid: {proc_id}\n"
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
 
     def __sort(self):
-
+        #TODO
+        """
+        if process.weight >= middle and process.weight < high:
+            self.warning.append(process.name)
+        elif process.weight >= high:
+            self.critical.append(process.name)
+        """
 
     def event_loop(self):
+        last_set = set()
+        while True:
+            proc_set = self._get_all()
+            diff = proc_set - last_set
+            last_set = proc_set
+            for proc in diff:
+                res = ""
+
+                base_info = self._get_name(proc)
+                connection = self._get_connection(proc)
+
+                res += base_info if base_info is not None else ""
+                res += connection if connection is not None else ""
+
+                #if "Network connection" in res:
+                #    print(res)
+                print(res)
+
 
 if __name__ == '__main__':
-    last_set = set()
-    while True:
-        proc_set = get_all_processes()
-        diff = proc_set - last_set
-        last_set = proc_set
-        print(proc_set)
-        # proc_set = set(proc_list)
-        for proc in diff:
-            res = ""
-            res += get_process_name(proc)
-            res += get_process_connection(proc)
-
-            print(res)
+    p = Process()
+    p.event_loop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
